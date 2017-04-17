e428c240672a26600b1d8d996a5b9b7856805cc9
kyubuns <kyubuns@gmail.com>
2017-04-17 10:12:15 +0900

    UnityPackage吐くスクリプト

---
+++ Unity/Assets/Editor/PackageExporter.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public static class PackageExporter
+{
+	[MenuItem("Baum2/Export Unity Package")]
+	public static void ExportUnityPackage()
+	{
+		var libraryDirectories = new[] {
+			"Assets/Baum2"
+		};
+
+		var outputFilePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "../baum2.unitypackage");
+
+		Debug.LogFormat("Export yokan package to {0}", outputFilePath);
+
+		AssetDatabase.ExportPackage(libraryDirectories, outputFilePath, ExportPackageOptions.Recurse);
+
+		Debug.LogFormat("Success");
+	}
+}
