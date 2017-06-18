51f21a60cf66409f55b85b52824b8af4bd23d514
kyubuns <kyubuns@gmail.com>
2017-06-18 14:12:31 +0900

    unitypackageリネーム

--- Unity/Assets/Editor/PackageExporter.cs
+++ Unity/Assets/Editor/PackageExporter.cs
@@ -4,14 +4,14 @@ using UnityEditor;
 
 public static class PackageExporter
 {
-	[MenuItem("Baum2/Export Unity Package")]
+	[MenuItem("Export/Export Unity Package")]
 	public static void ExportUnityPackage()
 	{
 		var libraryDirectories = new[] {
 			"Assets/Baum2"
 		};
 
-		var outputFilePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "../baum2.unitypackage");
+		var outputFilePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "../Baum2.unitypackage");
 
 		Debug.LogFormat("Export yokan package to {0}", outputFilePath);
 
