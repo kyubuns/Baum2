5ccd2c1eb64349088a3fe26e6085873088f9fbfb
kyubuns <kyubuns@gmail.com>
2018-10-02 11:25:34 +0900

    feat: Cache.Getで大文字小文字を区別しない

--- Unity/Assets/Baum2/Scripts/Cache.cs
+++ Unity/Assets/Baum2/Scripts/Cache.cs
@@ -39,22 +39,25 @@ namespace Baum2
 
         public GameObject Get(string path)
         {
-            var elements = path.Split(SplitChar);
+            if (path[0] == '/') path = path.Substring(1);
+            var elements = path.ToLower().Split(SplitChar);
             Array.Reverse(elements);
             Assert.AreNotEqual(elements.Length, 0, "Baum2.Cache.Get path.Length != 0");
 
-            var cand = List.FindAll(x => x.Name == elements[0]);
+            var cand = List.FindAll(x => x.Name.ToLower() == elements[0]);
             for (var i = cand.Count - 1; i >= 0; --i)
             {
                 var pass = true;
                 for (var j = 1; j < elements.Length; ++j)
                 {
-                    if (cand[i].Path.Length > j - 1 && cand[i].Path[j - 1] == elements[j]) continue;
+                    if (cand[i].Path.Length > j - 1 && cand[i].Path[j - 1].ToLower() == elements[j]) continue;
                     pass = false;
                     break;
                 }
+
                 if (!pass) cand.RemoveAt(i);
             }
+
             if (cand.Count == 0) return null;
             Assert.AreEqual(cand.Count, 1, string.Format("[Baum2] \"{0}\" is not unique", path));
             return cand[0].GameObject;
